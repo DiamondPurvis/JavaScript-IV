c432b11cc21d1f4bd74f572a00460708aad31c96
DiamondPurvis <purvisdiamond@gmail.com>
2019-01-24 22:56:13 -0500

    Created instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -21,6 +21,24 @@ class Person {
         this.gender = Person.gender
     }
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+//instructor//
+class Instructor extends Person {
+    constructor(Instructor){
+    super(Instructor);
+    this.specialty = Instructor.specialty;
+    this.favLanguage = Instructor.favLanguage;
+    this.catchPhrase = Instructor.catchPhrase;
+    }
+    //insert method//
+    demo(subject) {
+        return `Today we are learning about ${this.subject}`;
+    }
+    //insert method//
+    grade(student, subject) {
+        return `${student.name} recieves a perfect score on ${this.subject}`;
     }
 }
